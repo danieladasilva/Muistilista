c3ed72acd492af4516e387c1e9ef689b528f78d7
Daniela <dasilva.daniela@outlook.com>
2019-01-31 20:41:00 +0200

    kirjautumisominaisuus, polkujen suojaaminen

---
+++ application/tasks/forms.py
@@ -0,0 +1,10 @@
+
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, validators
+
+class TaskForm(FlaskForm):
+    name = StringField("Task name", [validators.Length(min=1, max=20)])
+    done = BooleanField("Done")
+  
+    class Meta:
+        csrf = False
