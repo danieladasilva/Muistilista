03c74b6dad2031ace3dde18312c3f7b9d645f18d
Daniela <dasilva.daniela@outlook.com>
2019-02-05 21:55:16 +0200

    projektin ulkoasun muokkausta, tietokantakyselyn tekeminen, abstraktin luokan Base tekeminen

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,13 +1,10 @@
 from application import db
+from application.models import Base
+from sqlalchemy.sql import text
 
-class User(db.Model):
+class User(Base):
 
     __tablename__ = "account"
-  
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
@@ -15,7 +12,7 @@ class User(db.Model):
 
     tasks = db.relationship("Task", backref='account', lazy=True)
 
-    def __init__(self, name, username, password):
+    def __init__(self, name):
         self.name = name
   
     def get_id(self):
@@ -29,3 +26,18 @@ class User(db.Model):
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def find_users_with_no_tasks(done=0):
+        stmt = text("SELECT Account.id, Account.name FROM Account"
+                     " LEFT JOIN Task ON Task.account_id = Account.id"
+                     " WHERE (Task.done IS null OR Task.done = :done)"
+                     " GROUP BY Account.id"
+                     " HAVING COUNT(Task.id) = 0").params(done=done)
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1]})
+
+        return response
