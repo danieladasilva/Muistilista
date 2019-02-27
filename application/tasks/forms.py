cf07a2c6e60375bb7d9ee26ffd7b6437e45115ed
Daniela <dasilva.daniela@outlook.com>
2019-02-27 17:53:59 +0200

    taskform muutokset takaisin

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -7,9 +7,9 @@ class TaskForm(FlaskForm):
     name = StringField("Task name", [validators.Length(min=2, max=20)])
     done = BooleanField("Done")
 
-   # groups_query = Group.query.all()
-   # lista = [(u.id, u.name) for u in groups_query]
-   # groups = SelectField(u'Group', choices=lista)
+    groups_query = Group.query.all()
+    lista = [(u.id, u.name) for u in groups_query]
+    groups = SelectField(u'Group', choices=lista)
 
     class Meta:
         csrf = False
